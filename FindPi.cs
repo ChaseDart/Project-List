e993682a6feec386985399a20a3cce80fda18bb4
ChronicRanga <56943448+ChronicRanga@users.noreply.github.com>
2020-01-20 11:17:26 +1000

    Add files via upload

---
+++ FindPi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number between 1 - 15 to find PI to the Nth Digit:");
+            string nth = Console.ReadLine();
+
+            Console.WriteLine("\n" + Math.Round(Math.PI, Convert.ToInt32(nth)));
+
+            Console.ReadLine();
+        }
+    }
+}
